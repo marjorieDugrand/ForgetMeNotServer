7a4eab0b064211eec00e10bda5432beb1de808e5
ronaldinha <vjm177@gmail.com>
2015-01-11 19:42:43 +0100

    add cors settings

--- router/index.js
+++ router/index.js
@@ -3,6 +3,11 @@ var users_routes = require('./routes/users');
 var contexts_routes = require('./routes/contexts');
 
 module.exports = function(app){
+	app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
 	app.use('/api/v1/tasks', tasks_routes);
 	app.use('/api/v1/users', users_routes);
 	app.use('/api/v1/contexts', contexts_routes);
